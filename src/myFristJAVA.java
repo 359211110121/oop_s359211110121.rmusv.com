7e9ef4ab54acc3e7fc511aec0831492d9668af88
359211110121 <patchayaphon14@gmail.com>
2017-11-17 10:31:43 +0700

    Initial upload to Github

---
+++ src/myFristJAVA.java
@@ -0,0 +1,10 @@
+package oop_lab.s359211110121.rmutsv.com;
+
+public class myFristJAVA {
+
+   public static void main(String []args){
+       System.out.println("Hello Word, I'm NEW");
+   } //main
+
+
+}// class
