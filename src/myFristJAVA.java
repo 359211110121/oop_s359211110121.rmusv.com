3d003570d9e4510a1aab675d7249c412f1c83125
s359211110121 <patchayaphon14@gmail.com>
2017-11-17 11:19:22 +0700

    testing edit code

--- src/myFristJAVA.java
+++ src/myFristJAVA.java
@@ -5,8 +5,7 @@ public class myFristJAVA {
    public static void main(String []args){
        System.out.println("Hello Word, I'm NEW");
        System.out.println("Information System");
-       System.out.println("Faculty of Management Technology");
-       System.out.println("RMUTSV");
+
    } //main
 
 
